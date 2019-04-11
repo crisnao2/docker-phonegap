253c0ea62b66054832c1cb59ed4f30630312f593
Cristiano Soares <crisnao2@gmail.com>
2019-04-11 16:42:33 -0300

    script to run container

---
+++ run-container.sh
@@ -0,0 +1,7 @@
+if [ -d "/dev/bus/usb" ] then
+    echo "Running container with usb mapped"
+    docker run --rm -it --name phonegap -v $PWD:/data --privileged -v /dev/bus/usb:/dev/bus/usb phonegap:latest /bin/bash
+else
+    echo "Running container with usb mapped"
+    docker run --rm -it --name phonegap -v $PWD:/data phonegap:latest /bin/bash
+fi
