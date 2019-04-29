44760f996ebc00141b09cfb28108b08fb6ea7030
Cristiano Soares <crisnao2@gmail.com>
2019-04-29 03:03:32 -0300

    Fix.

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,4 @@
-if [ -d "/dev/bus/usb" ] then
+if [ -d "/dev/bus/usb" ]; then
     echo "Running container with usb mapped"
     docker run --rm -it --name phonegap -v $PWD:/data --privileged -v /dev/bus/usb:/dev/bus/usb phonegap:latest /bin/bash
 else
